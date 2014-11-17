21bb57a90bb5e6b7268d71b2e34008332df46725
Tony Malins <tony.malins@contractcandles.com>
2014-11-17 13:13:11 +0000

    New Layout

--- js/init.js
+++ js/init.js
@@ -528,4 +528,21 @@
 
     });
 
+    var lastScrollTop = 0, delta = 5;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+
+       if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+       if (st > lastScrollTop){
+           // downscroll code
+           console.log('scroll down');
+       } else {
+          // upscroll code
+          console.log('scroll up');
+       }
+       lastScrollTop = st;
+    });
+
 })(jQuery);
